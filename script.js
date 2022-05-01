4bea40d8b78a12c303e6dbf8d2ec7cd1bcfcacbe
tyfvf <tyfvf2@gmail.com>
2022-05-01 16:56:44 -0300

    Style overhaul, made the css, title of the page displays bacons, now can see how many bacons per second and added a new building

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ let clickingPower = 1
 
 let cursorCost = 15
 let cursors = 0
-let farmerCost = 100
+let panCost = 100
+let pans = 0
+let farmerCost = 1000
 let farmers = 0
 
 function buyCursor() {
@@ -12,9 +14,23 @@ function buyCursor() {
         cursors ++
         cursorCost = Math.round(cursorCost * 1.15)
 
-        document.getElementById("bacons").innerHTML = bacons
-        document.getElementById("cursorcost").innerHTML = cursorCost
-        document.getElementById("cursors").innerHTML = cursors
+        document.getElementById("bacons").textContent = bacons
+        document.getElementById("cursorcost").textContent = cursorCost
+        document.getElementById("cursors").textContent = cursors
+        updateBaconsPerSecond()
+    }
+}
+
+function buyPan() {
+    if (bacons >= panCost) {
+        bacons -= panCost
+        pans ++
+        panCost = Math.round(panCost * 1.15)
+
+        document.getElementById("bacons").textContent = bacons
+        document.getElementById("pancost").textContent = panCost
+        document.getElementById("pans").textContent = pans
+        updateBaconsPerSecond()
     }
 }
 
@@ -24,19 +40,28 @@ function buyFarmer() {
         farmers ++
         farmerCost = Math.round(farmerCost * 1.15)
 
-        document.getElementById("bacons").innerHTML = bacons
-        document.getElementById("farmercost").innerHTML = farmerCost
-        document.getElementById("farmers").innerHTML = farmers
+        document.getElementById("bacons").textContent = bacons
+        document.getElementById("farmercost").textContent = farmerCost
+        document.getElementById("farmers").textContent = farmers
+        updateBaconsPerSecond()
     }
 }
 
 function addBacons(amount) {
     bacons += amount
-    document.getElementById("bacons").innerHTML = bacons
+    document.getElementById("bacons").textContent = bacons
+}
+
+function updateBaconsPerSecond() {
+    baconsPerSecond = cursors + pans * 5 + farmers * 50
+    document.getElementById('baconspersecond').textContent = baconsPerSecond
 }
 
 setInterval(function () {
     bacons += cursors
-    bacons += farmers * 5
-    document.getElementById("bacons").innerHTML = bacons
+    bacons += pans * 5
+    bacons += farmers * 50
+    document.getElementById("bacons").textContent = bacons
+
+    document.title = bacons + ' bacons - Pig Clicker'
 }, 1000) // 1sec
