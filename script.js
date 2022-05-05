2c466a58f956611ae8315361ae0500959282a791
tyfvf <tyfvf2@gmail.com>
2022-05-05 18:54:49 -0300

    Added a BUNCH of new content, for the game to fell more alive and also some small tweaks in the style

--- script.js
+++ script.js
@@ -25,15 +25,27 @@ let building = {
         'Cursor',
         'Pan',
         'Farmer',
-        'Factory'
+        'Factory',
+        'Laws and legislation',
+        'Alchemy',
+        'DNA replication',
+        'Javascript console'
     ],
     image: [
         'cursor.png',
         'pan.png',
         'farmer.png',
-        'factory.png'
+        'factory.png',
+        'laws-legislation.png',
+        'alchemy.png',
+        'dna.png',
+        'console.png'
     ],
     count: [
+        0,
+        0,
+        0,
+        0,
         0,
         0,
         0,
@@ -43,13 +55,21 @@ let building = {
         1,
         5,
         50,
-        125
+        125,
+        2,
+        1000,
+        9876,
+        999999
     ],
     cost: [
         15,
         100,
         1000,
-        9999
+        10250,
+        11111,
+        100000,
+        987654,
+        99999999
     ],
 
     purchase: function(index) {
@@ -66,46 +86,244 @@ let building = {
 
 let upgrade = {
     name: [
+        'Baby Fingers',
+        'Teen Fingers',
+        'Professional Fingers',
+        'Gold Fingers',
+        'Robotic Fingers',
         'Stone Clicker',
         'Iron Clicker',
-        'Baby Fingers',
+        'Diamond Clicker',
+        'Olive Oil free',
+        'No sticking',
+        'No gas',
+        'Sponsored Pig Food',
+        'Emotional Support',
+        'Pig Seeds!',
+        'Taylorism',
+        'Fordism',
+        'Toyotism',
+        'Eat Bacon',
+        'Only animal allowed in the world',
+        'The Piglegion',
+        'Gold',
+        'Wood',
+        'Air',
+        'A strong mutation',
+        'You have ascended'
     ],
     description: [
+        'Your Clicks worth twice as much you baby',
+        'Pretty confident hm? Your Clicks are 10 times more efficent',
+        'Your life is clickng a pig. Your clicks worth 100 times more',
+        'Holy moly that is what i call clicking. Your clicks worth 10 times more',
+        'Ok you are using auto clicker right?. 123 times more',
         'Cursors are twice as efficient',
         'Cursor are thrice as efficient',
-        'Your Clicks worth twice as much'
+        'Cursor are ten times as efficient',
+        'Pans are twice as efficent',
+        'Pans are thrice as efficient',
+        'Pans are ten times as efficient',
+        'Farmers are twice as efficent',
+        'Farmers are thrice as efficient',
+        'Farmers are ten times as efficient',
+        'Factories are twice as efficent',
+        'Factories are thrice as efficient',
+        'Factories are ten times as efficient',
+        'New law, everybody have to eat bacon in all meals, 100x bacons',
+        'New law, it is only allowed to have pigs, 10x bacons',
+        'New RELIGON, the Piglegion, 50x bacons',
+        'You can transform gold in bacon. Alchemy times 2',
+        'You can transform wood in bacon. Alchemy times 3',
+        'You can transform air in bacon. Alchemy times 10',
+        'All pigs have the tastiest bacons. DNA replication x10',
+        'game.bacons = 99999999, now this hole universe is in your hands.'
     ],
     image: [
+        'baby-fingers.png',
+        'teen-fingers.png',
+        'professional-fingers.png',
+        'gold-fingers.png',
+        'robotic-fingers.png',
         'stone-clicker.png',
         'iron-clicker.png',
-        'baby-fingers.png'
+        'diamond-clicker.png',
+        'olive-oil-free.png',
+        'no-sticking.png',
+        'no-gas.png',
+        'sponsored-food.jpeg',
+        'emotional-support.jpeg',
+        'pig-seeds.png',
+        'taylorism.jpeg',
+        'fordism.jpeg',
+        'toyotism.jpeg',
+        'eat-bacon.png',
+        'only-pig.jpeg',
+        'pig-religion.jpeg',
+        'gold.jpeg',
+        'wood.jpeg',
+        'air.jpeg',
+        'strong-mutation.jpeg',
+        'ascend.png'
     ],
     type: [
+        'click',
+        'click',
+        'click',
+        'click',
+        'click',
         'building',
         'building',
-        'click'
+        'building',
+        'building',
+        'building',
+        'building',
+        'building',
+        'building',
+        'building',
+        'building',
+        'building',
+        'building',
+        'building',
+        'building',
+        'building',
+        'building',
+        'building',
+        'building',
+        'building',
+        'building'
     ],
     cost: [
+        '350',
+        '4000',
+        '50000',
+        '600000',
+        '77777777',
         '300',
         '3000',
-        '350'
+        '30000',
+        '4000',
+        '40000',
+        '400000',
+        '50000',
+        '500000',
+        '5000000',
+        '600000',
+        '6000000',
+        '60000000',
+        '11111',
+        '22222222',
+        '3333333333',
+        '7000000',
+        '70000000',
+        '700000000',
+        '8000000000',
+        '99999999999',
     ],
     buildingIndex: [
+        -1,
+        -1,
+        -1,
+        -1,
+        -1,
         0,
         0,
-        -1
+        0,
+        1,
+        1,
+        1,
+        2,
+        2,
+        2,
+        3,
+        3,
+        3,
+        4,
+        4,
+        4,
+        5,
+        5,
+        5,
+        6,
+        7
     ],
     requirement: [
+        100,
+        1000,
+        10000,
+        100000,
+        1000000,
         10,
         50,
-        100
+        100,
+        10,
+        25,
+        50,
+        10,
+        20,
+        30,
+        10,
+        20,
+        30,
+        1,
+        20,
+        35,
+        5,
+        10,
+        15,
+        5,
+        1
     ],
     bonus: [
         2,
+        10,
+        100,
+        10,
+        123,
+        2,
         3,
-        2
+        10,
+        2,
+        3,
+        10,
+        2,
+        3,
+        10,
+        2,
+        3,
+        10,
+        100,
+        10,
+        50,
+        2,
+        3,
+        10,
+        10,
+        1
     ],
     purchased: [
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
         false,
         false,
         false
@@ -135,35 +353,119 @@ let upgrade = {
 let achievement = {
     name: [
         'A Humble Start',
+        'The middle of the Road',
+        'The Beginning of the end',
         'Click',
-        'The beginning of something'
+        'Clicks',
+        'Super Clicks',
+        'Clicktastic',
+        'Auto Clicker',
+        'The beginning of something',
+        'Huh a fresh bacon save a slice',
+        'Ok',
+        'That is ok',
+        'That is ok stop',
+        'STOP',
+        'ITS ENOUGH BACON'
     ],
     description: [
         'Buy 1 cursor',
+        'Buy 1 Laws and Legislation',
+        'Buy 1 Javascript Console',
         'Click the pig 1 time',
-        'Gather 1 bacon'
+        'Click the pig 1000 time',
+        'Click the pig 10000 time',
+        'Click the pig 100000 time',
+        'Click the pig 1000000 time',
+        'Gather 1 bacon',
+        'Gather 1000 bacons',
+        'Gather 100000 bacons',
+        'Gather 1000000 bacons',
+        'Gather 100000000 bacons',
+        'Gather 1000000000 bacons',
+        'Gather 99999999999 bacons'
     ],
     image: [
         'humble-start.png',
+        'middle-road.png',
+        'beginning-end.png',
         'click.png',
-        'bacon.png'
+        'clicks.png',
+        'super-clicks.png',
+        'clicktastic.png',
+        'auto-clicker.jpeg',
+        'bacon.png',
+        'fresh-bacon.png',
+        'ok.png',
+        'that-ok.png',
+        'that-ok-stop.png',
+        'stop.png',
+        'enough.png'
     ],
     type: [
         'building',
+        'building',
+        'building',
         'click',
+        'click',
+        'click',
+        'click',
+        'click',
+        'bacons',
+        'bacons',
+        'bacons',
+        'bacons',
+        'bacons',
+        'bacons',
         'bacons'
     ],
     requirement: [
         1,
         1,
-        1
+        1,
+        1,
+        1000,
+        10000,
+        100000,
+        1000000,
+        1,
+        1000,
+        100000,
+        1000000,
+        100000000,
+        1000000000,
+        99999999999
     ],
     objectIndex: [
         0,
+        4,
+        7,
+        -1,
+        -1,
+        -1,
+        -1,
+        -1,
+        -1,
+        -1,
+        -1,
+        -1,
+        -1,
         -1,
         -1
     ],
     awarded: [
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
         false,
         false,
         false
@@ -184,7 +486,7 @@ let display = {
     updateShop: function() {
         document.getElementById('shopContainer').innerHTML = ""
         for (i = 0; i < building.name.length; i++) {
-            document.getElementById('shopContainer').innerHTML += '<table class="shopButton" onclick="building.purchase('+i+')"><tr><td id="image"><img src="images/'+building.image[i]+'" alt="a photo of a cursor, click to buy"></td><td id="nameAndCost"><p>'+building.name[i]+'</p><p><span>'+building.cost[i]+'</span> bacons</p></td><td id="amount"><span>'+building.count[i]+'</span></td></tr></table>'
+            document.getElementById('shopContainer').innerHTML += '<table class="shopButton" onclick="building.purchase('+i+')"><tr><td id="image"><img src="images/buildings/'+building.image[i]+'" alt="a photo of a cursor, click to buy"></td><td id="nameAndCost"><p>'+building.name[i]+'</p><p><span>'+building.cost[i]+'</span> bacons</p></td><td id="amount"><span>'+building.count[i]+'</span></td></tr></table>'
         }
     },
 
@@ -193,9 +495,9 @@ let display = {
         for (i = 0; i < upgrade.name.length; i++) {
             if (!upgrade.purchased[i]) {
                 if (upgrade.type[i] == 'building' && building.count[upgrade.buildingIndex[i]] >= upgrade.requirement[i]) {
-                    document.getElementById('upgradeContainer').innerHTML += '<img src="images/'+upgrade.image[i]+'" title="'+upgrade.name[i]+'&#10;'+upgrade.description[i]+'&#10;('+upgrade.cost[i]+' bacons)" onclick="upgrade.purchase('+i+')">'
+                    document.getElementById('upgradeContainer').innerHTML += '<img src="images/upgrades/'+upgrade.image[i]+'" title="'+upgrade.name[i]+'&#10;'+upgrade.description[i]+'&#10;('+upgrade.cost[i]+' bacons)" onclick="upgrade.purchase('+i+')">'
                 } else if (upgrade.type[i] == 'click' && game.totalClicks >= upgrade.requirement[i]) {
-                    document.getElementById('upgradeContainer').innerHTML += '<img src="images/'+upgrade.image[i]+'" title="'+upgrade.name[i]+'&#10;'+upgrade.description[i]+'&#10;('+upgrade.cost[i]+' bacons)" onclick="upgrade.purchase('+i+')">'
+                    document.getElementById('upgradeContainer').innerHTML += '<img src="images/upgrades/'+upgrade.image[i]+'" title="'+upgrade.name[i]+'&#10;'+upgrade.description[i]+'&#10;('+upgrade.cost[i]+' bacons)" onclick="upgrade.purchase('+i+')">'
                 }
             }
         }
@@ -205,7 +507,7 @@ let display = {
         document.getElementById('achievementContainer').innerHTML = ""
         for (i = 0; i < achievement.name.length; i++) {
             if (achievement.awarded[i]) {
-                document.getElementById('achievementContainer').innerHTML += '<img src="images/'+achievement.image[i]+'" title="'+achievement.name[i]+'&#10;'+achievement.description[i]+'">'
+                document.getElementById('achievementContainer').innerHTML += '<img src="images/achievements/'+achievement.image[i]+'" title="'+achievement.name[i]+'&#10;'+achievement.description[i]+'">'
             }
         }
     }
@@ -291,7 +593,7 @@ function createNumberOnClick(event) {
     // Create the number with img
     let element = document.createElement('div')
     element.textContent = '+' + game.clickValue
-    element.innerHTML += '<img src="images/bacon.png" height="24px" width="24px">'
+    element.innerHTML += '<img src="images/achievements/bacon.png" height="24px" width="24px">'
     element.classList.add('number', 'unselectable')
     element.style.left = position.x + 'px'
     element.style.top = position.y + 'px'
